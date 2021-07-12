b8019a056024380c81d80289b9863f3bcec9d51d
Md. Shihab <shihabrayhan8@gmail.com>
2021-07-13 00:19:32 +0600

    Snake has been created!

---
+++ index.js
@@ -0,0 +1,44 @@
+const board_border = 'black';
+const board_background = 'white';
+const snake_col = 'lightblue';
+const snake_border = 'darkblue';
+
+//get the canvas element
+const snakeboard = document.getElementById('myCanvas');
+//return a two dimentional element
+const snakeboard_ctx = snakeboard.getContext('2d');
+
+let snake = [
+    { x: 200, y: 200},
+    { x: 190, y: 200},
+    { x: 180, y: 200},
+    { x: 170, y: 200},
+    { x: 160, y: 200}
+];
+
+
+//start game
+main();
+
+function main() {
+    drawSnake();
+}
+
+
+//draw the snake part
+function drawSnakePart(snakePart)
+{
+    //select the color to fill the drawing
+    snakeboard_ctx.fillStyle = 'lightblue';
+    //select the color to fill the border of the snake
+    snakeboard_ctx.strokeStyle = 'darkblue';
+    //draw a fill rectangle to cover the entire snake
+    snakeboard_ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
+    //draw the border of the snake
+    snakeboard_ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+}
+//draw the snake on the canvas
+function drawSnake() {
+    //draw each part
+    snake.forEach(drawSnakePart);
+}
